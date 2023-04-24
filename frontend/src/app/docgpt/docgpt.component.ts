af12e2207d9ca17fa5183586d9eca97bba734a5f
bredda <mangin.laurent@gmail.com>
2023-04-24 09:38:36 +0200

    front: dimish font-size

--- frontend/src/app/docgpt/docgpt.component.ts
+++ frontend/src/app/docgpt/docgpt.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ActivatedRoute } from '@angular/router';
 import { ContextService } from './services/context.service';
@@ -13,22 +20,42 @@ import { Subscription } from 'rxjs';
 export class DocgptComponent implements OnInit, OnDestroy {
   currentProject = undefined;
   currentChat = undefined;
+  private subscriptions: Subscription[] = [];
   paramsSubscription!: Subscription;
+  @ViewChild('target', { static: false })
+  set viewChildReference(v: ElementRef) {
+    console.log(v);
+    if (v !== undefined) {
+      v.nativeElement.scroll({
+        top: v.nativeElement.scrollHeight,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+
   constructor(
     private route: ActivatedRoute,
     private contextService: ContextService
   ) {}
 
   ngOnInit(): void {
-    this.paramsSubscription = this.route.params.subscribe((paramMap) => {
-      this.contextService.triggerContextChange(
-        paramMap['projectId'],
-        paramMap['chatId']
-      );
-    });
+    this.subscriptions.push(
+      this.route.params.subscribe((paramMap) => {
+        this.contextService.triggerContextChange(
+          paramMap['projectId'],
+          paramMap['chatId']
+        );
+      })
+    );
+    this.subscriptions.push(
+      this.contextService.listenToDataChange().subscribe((v) => {
+        window.scrollTo(0, document.body.scrollHeight);
+      })
+    );
   }
 
   ngOnDestroy(): void {
-    this.paramsSubscription.unsubscribe();
+    this.subscriptions.forEach((s) => s.unsubscribe());
   }
 }
