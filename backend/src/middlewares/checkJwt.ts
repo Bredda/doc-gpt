cf93fc95f78365b326ddc30658176da48b74de4e
bredda <mangin.laurent@gmail.com>
2023-04-24 14:14:38 +0200

    back: check user exists for valid token

--- backend/src/middlewares/checkJwt.ts
+++ backend/src/middlewares/checkJwt.ts
@@ -1,8 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import config from '../config/config';
+import { getUserById } from '../domain/repositories/user.repository';
 
-export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
+export const checkJwt = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   //Get the jwt token from the head
   let token = <string>req.headers['authorization'];
   let jwtPayload;
@@ -16,6 +21,7 @@ export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
   try {
     jwtPayload = <any>jwt.verify(token, config.jwtSecret);
     res.locals.jwtPayload = jwtPayload;
+    await getUserById(jwtPayload.userId);
   } catch (error) {
     //If token is not valid, respond with 401 (unauthorized)
     res.status(401).send();
